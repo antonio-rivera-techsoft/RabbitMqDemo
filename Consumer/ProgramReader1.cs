0f45d119da3fd1054411bfa4123407752f374f77
Antonio Rivera <antonio.rivera@techsoft.com.mx>
2024-10-24 11:29:55 -0700

    Agregar archivos de proyecto.

---
+++ Consumer/ProgramReader1.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Consumer
+{
+    internal class ProgramReader1
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "DemoQueue", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    Task.Delay(5000).Wait();
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine("Message Received: ", message);
+                    channel.BasicAck(ea.DeliveryTag, false);
+                };
+                channel.BasicConsume(queue: "DemoQueue", autoAck: true, consumer: consumer);
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Producer/ProgramProducer.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+namespace Producer
+{
+    internal class ProgramProducer
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+
+            string exchangeName = "DemoExchange";
+            string routingKey = "routing-key";
+            string queueName = "DemoQueue";
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+                channel.QueueDeclare(queueName, false, false, false, null);
+                channel.QueueBind(queueName, exchangeName, routingKey, null);
+
+                for (int i = 0; i < 60; i++)
+                {
+                    var body = Encoding.UTF8.GetBytes($"Sending Message: {i}");
+                    channel.BasicPublish(exchange: "DemoExchange", routingKey: "routing-key", basicProperties: null, body: body);
+                    Console.WriteLine($"Sending Message: {i}");
+                    Thread.Sleep(1000);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
